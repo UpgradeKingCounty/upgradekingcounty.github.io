28d40122ef2ca45074aa45aab7b6ff9370a6f6eb
Daniel Worthington <danielworthington@gmail.com>
2021-05-24 17:24:50 -0700

    Footer design changes, including new logo (#12)
    * Rename hero image for clarity and made minor adjustments to the position of the text.
    
    * Set an image to be used when sharing links on social media sites.
    - Set an open graph image URL for a large logo image.
    - Changed the site description in the open graph metadata to match the home page.
    - Added titles and open graph metadata to the About and Update pages.
    
    * Fix spelling error.
    
    * Updated footer to new design. Replaced STC logo with newer one.

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,34 +1,51 @@
 import React from "react";
-import STCLogo from "../../images/share-the-cities-logo.png";
-import "./footer.css";
+import STCLogo from "../../images/stc-logo.png";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="footer-container">
-      <div>
-        <img src={STCLogo} alt="Share the Cities Logo" />
-      </div>
-      <div className="footer-link-container">
-        <div className="footer-org-column">
-          <h2>Share the Cities</h2>
-          <a href="https://sharethecities.org">About</a>
-          <a href="https://twitter.com/sharethecities">Twitter</a>
+    <footer className="mt-8 py-8 px-5 sm:px-8 border-t-2 border-blue-dark text-sm text-gray-600">
+      <div className="flex items-center">
+        <a href="https://sharethecities.org">
+          <img
+            className="w-32"
+            src={STCLogo}
+            alt="Share the Cities Action Fund"
+          />
+        </a>
+        <div className="ml-8 md:ml-24">
+          <h3 className="font-bold">Share the Cities</h3>
+          <a className="block underline" href="https://sharethecities.org">
+            About
+          </a>
+          <a
+            className="block underline"
+            href="https://twitter.com/sharethecities"
+          >
+            Twitter
+          </a>
         </div>
-        <div className="footer-org-column">
-          <h2>Contribute</h2>
-          <a href="https://sharethecities.org/contact-1">Contact</a>
-          <a href="https://github.com/UpgradeKingCounty/upgradekingcounty.github.io">
+        <div className="ml-8 md:ml-24">
+          <h3 class="font-bold">Contribute</h3>
+          <a
+            className="block underline"
+            href="https://sharethecities.org/contact-1"
+          >
+            Contact
+          </a>
+          <a
+            className="block underline"
+            href="https://github.com/UpgradeKingCounty/upgradekingcounty.github.io"
+          >
             GitHub
           </a>
         </div>
       </div>
-      <hr />
-      <div className="footer-copyright-container">
-        <p>Copyright © {currentYear} Upgrade King County</p>
-      </div>
-    </div>
+      <p className="mt-4 text-center text-xs">
+        Copyright © {currentYear} Upgrade King County
+      </p>
+    </footer>
   );
 }
 
